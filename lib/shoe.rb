7a3e9e8f03d6e1e7da8b031d0d90f3a33e0f2864
richietam <richardtam.ny@gmail.com>
2019-02-24 22:56:40 -0500

    finished lab

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,23 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS.push(brand) unless BRANDS.include?(brand)
   end
+
+  def brand=(brand)
+    @brand = brand
+  end
+
+  def self.brands
+    BRANDS.uniq
+  end
+
 
   def cobble
     self.condition = "new"
