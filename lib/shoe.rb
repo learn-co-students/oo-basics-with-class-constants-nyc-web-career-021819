cc49ea4943ce2963a1480fa03b51bf51e01f06c9
Lisa Scott <lisarenesoctt@gmail.com>
2019-04-23 17:23:35 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,20 @@
+# class Shoe
+#   attr_accessor :brand
+#   attr_reader :color, :size, :material, :condition
+#   BRANDS = []
+#   def initialize(brand)
+#     @brand = brand
+#     #if !BRANDS.include?(brand)
+#       BRANDS << brand
+#
+#   end
+#
+#   def coble
+#     self.condition = "new"
+#     puts "Your shoe is as good as new!"
+#   end
+# end
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
